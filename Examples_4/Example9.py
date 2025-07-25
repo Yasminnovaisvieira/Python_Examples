78c2eb2fadf187402dcb4dbb88e15bcc1715fc74
Yasminnovaisvieira <Yasminnovaisvieira789@gmail.com>
2025-07-25 13:19:49 -0300

    Python - Examples

--- Examples_4/Example9.py
+++ Examples_4/Example9.py
@@ -7,12 +7,17 @@ def verificar_palavras(lista_palavras):
         if palavra[0] == 'a' or palavra[0] == 'A':
             contador += 1
 
-    print(f"A quantidade de palavras que começam com a letra 'A' é: {contador}")
+    print(f"\nA quantidade de palavras que começam com a letra 'A' é: {contador}")
 
     return contador
 
 
+tamanho_lista = int(input("Digite o tamanho da lista: "))
+lista_palavras = []
 
-lista_palavras = ["amor", "feijão", "ARROZ"]
+for palavra_lista in range(0, tamanho_lista):
+    palavra_lista = str(input( f"Digite a {palavra_lista + 1}ª palavra da lista: "))
+
+    lista_palavras.append(palavra_lista)
 
 verificar_palavras(lista_palavras)
